53e75f80a8eb4bc15a535b755aee8e0deba878fd
Alberto Fustinoni <alberto-fustinoni@outlook.com>
2018-11-19 15:34:06 +0900

    Build

---
+++ downloadArtifacts.ps1
@@ -0,0 +1,24 @@
+$apiUrl = 'https://ci.appveyor.com/api'
+$headers = @{
+    'Content-type' = 'application/json'
+}
+
+$downloadDirectory = "Build"
+$account = "Aftnet"
+$project = "ffmpegwin"
+$projectObject = Invoke-RestMethod -Method Get -Uri "$apiUrl/projects/$account/$project" -Headers $headers
+
+foreach($i in $projectObject.build.jobs) {
+    $jobId = $i.jobId
+    $artifacts = Invoke-RestMethod -Method Get -Uri "$apiUrl/buildjobs/$jobId/artifacts" -Headers $headers
+    foreach($j in $artifacts) {
+        if($j.type -eq "Zip") {
+            $artifactUrl = "$apiUrl/buildjobs/$jobId/artifacts/$($j.fileName)"
+            $localArtifactPath = Join-Path $downloadDirectory $j.fileName
+            echo "Downloading $artifactUrl to $localArtifactPath"
+            Invoke-RestMethod -Method Get -Uri $artifactUrl -OutFile $localArtifactPath -Headers $headers
+            $localArtifactDir = $localArtifactPath.Replace(".zip", "")
+            Expand-Archive -Path $localArtifactPath -DestinationPath $localArtifactDir
+        }
+    }
+}
